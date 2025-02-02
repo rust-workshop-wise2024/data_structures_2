199fa9fde5dabcdde0bb5d1023990b7546105615
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2025-02-02 08:26:41 +0100

    Solution aufgabe 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::f64::consts::PI;
+
+trait Shape {
+    fn area(&self) -> f64;
+    fn perimeter(&self) -> f64;
+}
+
+struct Circle {
+    radius: f64,
+}
+
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        (PI * self.radius).powi(2)
+    }
+    fn perimeter(&self) -> f64 {
+        2.0 * (PI * self.radius)
+    }
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.height * self.width
+    }
+    fn perimeter(&self) -> f64 {
+        2.0 * (self.height + self.width)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let circle: Circle = Circle { radius: 5.0 };
+    let rectangle: Rectangle = Rectangle { width: 4.0, height: 7.0 };
+
+    println!("Circle Perimeter: {:.2}, Area: {:.2}", circle.perimeter(), circle.area());
+    println!("Rectangle Perimeter: {:.2}, Area: {:.2}", rectangle.perimeter(), rectangle.area());
 }
