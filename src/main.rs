148e64ce4f869049c466846bdccf33235c52b965
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2025-02-02 08:13:53 +0100

    finished implementation of Aufgabe1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+trait Animal {
+    fn greet(&self) -> String;
+}
+
+struct Cat {
+    name: String,
+}
+
+struct Dog {
+    name: String,
+}
+
+impl Animal for Dog {
+    fn greet(&self) -> String {
+        String::from("rof rof rof")
+    }
+}
+
+impl Animal for Cat {
+    fn greet(&self) -> String {
+        String::from("meow meow")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let max = Dog { name: String::from("Max") };
+    println!("Max (Hund) sagt {}", max.greet());
+
+    let luna = Cat { name: String::from("Luna") };
+    println!("Luna (Katze) sagt {}", luna.greet());
 }
